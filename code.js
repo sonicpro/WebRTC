bd58165cf5d36b67dc18f6fbe79e383fec2c57fe
Volodymyr Tsurko <volodymyr.tsurko@qognify.com>
2025-02-06 15:01:26 +0200

    Added getting video stream.

--- code.js
+++ code.js
@@ -1,1 +1,22 @@
-const video = window.navigator.getUserMedia(
+let stream;
+let interval;
+const video = window.navigator.getUserMedia({ "video": true }, (vid) =>{
+  stream = vid;
+  interval = startTimer();
+}, logError);
+
+function startTimer() {
+  return setTimeout(() => {
+    const tracks = stream.getTracks();
+    tracks.forEach((track) => {
+      track.stop();
+    });
+    stream = null;
+    clearInterval(interval);
+    console.log('stopped');
+  }, 5000);
+}
+
+function logError(error) {
+    console.log(error.name + ": " + error.message);
+}
